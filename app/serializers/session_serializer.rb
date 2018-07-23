3a118a6e2a374c832471feae4f3190ebea40347c
mcmht507 <mcmht507@gmail.com>
2018-07-24 01:52:12 +0900

    add role_type response

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,6 +1,6 @@
 class SessionSerializer < ActiveModel::Serializer
 
-  attributes :email, :token_type, :user_id, :access_token,:updated_at, :created_at
+  attributes :email, :token_type, :user_id, :role_type, :access_token, :created_at, :updated_at
 
   # def user_id
   #   object.id
