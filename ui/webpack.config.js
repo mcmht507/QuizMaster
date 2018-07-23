63809cc57e0e5dd19c51cb5143829d5abe3c3c86
mcmht507 <mcmht507@gmail.com>
2018-07-24 01:08:13 +0900

    delete comment

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,42 +1,13 @@
 const webpack = require('webpack')
 const path = require('path');
 
-
-
-// module.exports = {
-//   // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-//   mode: 'development',
-//   // エントリーポイントの設定
-//   entry: [
-//     // './src/index.js'
-//   ],
-//   // 出力の設定
-//   output: {
-//     // 出力するファイル名
-//     filename: 'bundle.js',
-//     // 出力先のパス（v2系以降は絶対パスを指定する必要がある）
-//     path: path.join(__dirname, 'public/js')
-//   },
-//   module: {
-//     rules: [{
-//       test: /\.js?$/,
-//       exclude: /node_modules/,
-//       loader: 'babel-loader',
-//       query: {
-//         plugins: ["transform-react-jsx", "babel-plugin-transform-decorators-legacy"]
-//       }
-//     }]
-//   }
-// };
-
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map', // ソースマップファイル追加 
+  devtool: 'inline-source-map',
   entry: [
     'babel-polyfill',
     'react-hot-loader/patch',
-    './src/index.js', // エントリポイントのjsxファイル
-    // __dirname + 'src/index', // エントリポイントのjsxファイル
+    './src/index.js',
   ],
   // React Hot Loader用のデバッグサーバ(webpack-dev-server)の設定
   devServer: {
@@ -63,14 +34,14 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
-    new webpack.NamedModulesPlugin(), // 名前変更無効プラグイン利用
-    new webpack.HotModuleReplacementPlugin() // HMR(Hot Module Reload)プラグイン利用 
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [{
-      test: /\.js?$/, // 拡張子がjsで
-      exclude: /node_modules/, // node_modulesフォルダ配下は除外
-      include: __dirname,// プロジェクトディレクトリ配下のJSファイルが対象
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      include: __dirname,
       use: {
         loader: 'babel-loader',
         options: {
